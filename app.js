02ea62fe285495173301ef5df6c7e710d8ce91cc
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-09 12:54:05 -0800

    headDropdown event listener now renders the selected head value

--- app.js
+++ app.js
@@ -20,12 +20,13 @@ const catchphrases = [];
 // event listeners
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
-
+    let headSelection = headDropdown.value;
     // increment the head change count state
     head++;
-    // update the dom for the head (use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
+    headEl.style.backgroundImage = `url(assets/${headSelection}-head.png)`;
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 
@@ -37,6 +38,7 @@ middleDropdown.addEventListener('change', () => {
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
 
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 
@@ -48,6 +50,7 @@ bottomDropdown.addEventListener('change', () => {
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
 
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 catchphraseButton.addEventListener('click', () => {
